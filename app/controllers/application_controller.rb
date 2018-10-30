95096746bbba79f0132415feb5246638dfc13386
Blud7 <paul.riviere.pr@gmail.com>
2018-10-30 14:34:12 +0100

    push heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+	def sign_in
+
+  	@gossips = Gossip.all
+	@gossips.map do |key|
+
+	  	@author = key.author
+	  	@content = key.content
+
+	  	puts "Voilà ce que #{@author} a balancé : #{@content}"
+		end
+	end
 end
