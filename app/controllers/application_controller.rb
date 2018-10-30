51fa09ee30242fc545bbe35d07f9fb8f802be22b
Blud7 <paul.riviere.pr@gmail.com>
2018-10-30 15:42:34 +0100

    added everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,7 @@ class ApplicationController < ActionController::Base
 	def sign_in
 
   	@gossips = Gossip.all
-	@gossips.map do |key|
-
-	  	@author = key.author
-	  	@content = key.content
 
 	  	puts "Voilà ce que #{@author} a balancé : #{@content}"
-		end
 	end
 end
